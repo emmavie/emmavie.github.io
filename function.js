7566b1ad65f6ea650f3d7fd503f7081e7106ddab
emmavie <75275724+emmavie@users.noreply.github.com>
2022-06-20 21:37:06 -0400

    contact me change

--- function.js
+++ function.js
@@ -1,3 +1,39 @@
 ScrollReveal().reveal(".view",{ delay: 400 });
 ScrollReveal().reveal(".scroll2",{ delay: 800 });
 ScrollReveal().reveal(".scroll3",{ delay: 1400 });
+
+   const constraints = {
+       name: {
+           presence: { allowEmpty: false }
+       },
+       email: {
+           presence: { allowEmpty: false },
+           email: true
+       },
+       message: {
+           presence: { allowEmpty: false }
+       }
+   };
+
+   const form = document.getElementById('contact-form');
+
+   form.addEventListener('submit', function (event) {
+     const formValues = {
+         name: form.elements.name.value,
+         email: form.elements.email.value,
+         message: form.elements.message.value
+     };
+
+     const errors = validate(formValues, constraints);
+
+     if (errors) {
+       event.preventDefault();
+       const errorMessage = Object
+           .values(errors)
+           .map(function (fieldValues) { return fieldValues.join(', ')})
+           .join("\n");
+
+       alert(errorMessage);
+     }
+   }, false);
+
